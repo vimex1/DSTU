9e82de30514324bec3d4a27ac9ea950ae7dd4a27
vimex1 <154634383+vimex1@users.noreply.github.com>
2023-12-22 23:46:56 +0300

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Book(ABC):
+    '''Класс использющий абстрактный метод get_summary'''
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def get_summary(self):
+        pass
+
+class Fiction(Book):
+    def get_summary(self):
+        print(f'"{self.title}" - роман в стиле исторический фикшн, автор - {self.author}')
+
+class NonFiction(Book):
+    def get_summary(self):
+        print(f'"{self.title}" - книга в стиле нон фикшн, автор - {self.author}')
+
+class Poetry(Book):
+    pass
